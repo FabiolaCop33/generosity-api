37f6598c275f32d79037791f8f5e45121cb44a80
Fabiola Correa Padilla <129307826+FabiolaCop33@users.noreply.github.com>
2024-10-29 21:49:20 -0600

    First commit

---
+++ src/main/java/com/generosity/api/GenerosityApiApplication.java
@@ -0,0 +1,13 @@
+package com.generosity.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerosityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerosityApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generosity/api/GenerosityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generosity.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerosityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
